5f76e086f2685b53058859c4615b0caf483fb78e
wiyw <quantumdragon8@gmail.com>
2026-03-30 16:26:32 -0700

    Auto-rebuild: 2026-03-30 16:26:32

--- home/ags.nix
+++ home/ags.nix
@@ -3,10 +3,18 @@
 {
   # Install AGS and required dependencies
   home.packages = with pkgs; [
-    ags
-    esbuild      # REQUIRED: Compiles TS/TSX into JS on the fly
-    dart-sass    # Useful if we ever switch from CSS to SCSS
-    wireplumber  # For controlling audio later
+    # Wrapped AGS with Astal modules injected so it can read system state
+    (ags.override {
+      extraPackages = [
+        astal.network
+        astal.wireplumber
+        astal.bluetooth
+      ];
+    })
+    esbuild        # REQUIRED: Compiles TS/TSX into JS on the fly
+    dart-sass      # Useful if we ever switch from CSS to SCSS
+    wireplumber    # For controlling audio later
+    brightnessctl  # Required for the brightness slider script
   ];
 
   # Symlink our custom folder to the system config path
