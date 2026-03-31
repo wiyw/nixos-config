86bf9a3321e3da23786d68252fb8370a6c745a31
wiyw <quantumdragon8@gmail.com>
2026-03-31 13:04:36 -0700

    Auto-rebuild: 2026-03-31 13:04:36

--- modules/ags/ControlCenter.tsx
+++ modules/ags/ControlCenter.tsx
@@ -49,8 +49,6 @@ const NightLightPopup = ({ onClose }: { onClose: () => void }) => {
                 </button>
             </box>
             <Gtk.Scale 
-                min={1000} 
-                max={10000} 
                 value={temp} 
                 onValueChanged={(self) => setTemp(self.get_value())} 
                 hexpand 
@@ -181,11 +179,11 @@ export default function ControlCenterWindow() {
                 <box cssClasses={["tn-panel"]} orientation={Gtk.Orientation.VERTICAL} spacing={10}>
                     <box spacing={10}>
                         <label cssClasses={["slider-icon"]} label="󰝀 " />
-                        <Gtk.Scale hexpand min={0} max={100} value={volume} onValueChanged={(self) => setVolume(self.get_value())} />
+                        <Gtk.Scale hexpand value={volume} onValueChanged={(self) => setVolume(self.get_value())} />
                     </box>
                     <box spacing={10}>
                         <label cssClasses={["slider-icon"]} label="󰛨 " />
-                        <Gtk.Scale hexpand min={0} max={100} value={brightness} onValueChanged={(self) => setBrightness(self.get_value())} />
+                        <Gtk.Scale hexpand value={brightness} onValueChanged={(self) => setBrightness(self.get_value())} />
                     </box>
                 </box>
 
