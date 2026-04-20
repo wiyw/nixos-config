541105818e2f11a4d344f2ebe701be77959bdd88
wiyw <quantumdragon8@gmail.com>
2026-04-19 18:59:44 -0700

    Auto-rebuild: 2026-04-19 18:59:44

--- modules/network.nix
+++ modules/network.nix
@@ -1,6 +1,6 @@
-{ inputs, config, pkgs, ... }: {
+{ inputs, config, pkgs, lib, ... }@inputs:
 
-  # Use the flake input directly
+{
   imports = [ inputs.sops-nix.homeManagerModules.sops ];
 
   sops = {
@@ -9,10 +9,8 @@
     secrets.warp_private_key = {};
   };
 
-  # 1. We define the file ONCE and use the path from SOPS
   home.file.".config/wireguard/warp.conf".text = ''
     [Interface]
-    # This path is managed by sops-nix
     PrivateKey = $(cat ${config.sops.secrets.warp_private_key.path})
     Address = 172.16.0.2/32, 2606:4700:110:853b:f00d:78f0:f0ba:cb06/128
     DNS = 1.1.1.1
@@ -23,21 +21,5 @@
     AllowedIPs = 0.0.0.0/0, ::/0
   '';
 
-  home.packages = [ pkgs.wireguard-tools pkgs.wgcf ];
-
-  systemd.user.services.warp-vpn = {
-    Unit = {
-      Description = "Cloudflare WARP WireGuard Tunnel";
-      After = [ "network.target" ];
-    };
-    Service = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      # 2. Note: Executing wg-quick as a user usually requires 
-      # specific sudo rules in your NixOS configuration.
-      ExecStart = "${pkgs.sudo}/bin/sudo ${pkgs.wireguard-tools}/bin/wg-quick up %h/.config/wireguard/warp.conf";
-      ExecStop = "${pkgs.sudo}/bin/sudo ${pkgs.wireguard-tools}/bin/wg-quick down %h/.config/wireguard/warp.conf";
-    };
-    Install = { WantedBy = [ "default.target" ]; };
-  };
+  home.packages = [ pkgs.wgcf ];
 }
